6734c4f3e13ebf60cd4befda2f117a64afa2c9ef
Kevin <tokyob0t.business@proton.me>
2025-07-16 23:08:17 -0400

    chore(deps)!: updated Variable class

--- astal/binding.lua
+++ astal/binding.lua
@@ -1,19 +1,24 @@
-local lgi = require("lgi")
-local GObject = lgi.require("GObject", "2.0")
+local lgi = require('lgi')
+local GObject = lgi.require('GObject', '2.0')
 
----@class Binding
----@field emitter table | Variable | userdata
+---@class AstalLuaBinding: table
+---@field emitter table | GObject.Object
 ---@field property? string
 ---@field transform_fn function
----@field private __index Binding
----@overload fun(emitter: table | userdata, property?: string): Binding
+---@field private __index AstalLuaBinding
+---@overload fun(emitter: table | userdata, property?: string): AstalLuaBinding
 local Binding = {}
 Binding.__index = Binding ---@diagnostic disable-line
 
 ---@param emitter table | Variable | userdata
 ---@param property? string
----@return Binding
 function Binding.new(emitter, property)
+    local Variable = require('astal.variable')
+
+    if Variable:is_type_of(emitter) then
+        property = 'value'
+    end
+
     return setmetatable({
         emitter = emitter,
         property = property,
@@ -25,26 +30,25 @@ end
 
 ---@private
 function Binding:__tostring()
-    local str = "Binding<" .. tostring(self.emitter)
+    local str = 'Binding<' .. tostring(self.emitter)
     if self.property ~= nil then
-        str = str .. ", " .. self.property
+        str = str .. ', ' .. self.property
     end
-    return str .. ">"
+    return str .. '>'
 end
 
 ---@return any
 function Binding:get()
     if self.property ~= nil and GObject.Object:is_type_of(self.emitter) then
         return self.transform_fn(self.emitter[self.property])
-    elseif type(self.emitter.get) == "function" then
+    elseif type(self.emitter.get) == 'function' then
         return self.transform_fn(self.emitter:get())
     else
-        error("can not get: Not a GObject or a Variable " .. self)
+        error('can not get: Not a GObject or a Variable ' .. self)
     end
 end
 
 ---@param transform fun(value: any): any
----@return Binding
 function Binding:as(transform)
     local b = Binding.new(self.emitter, self.property)
     b.transform_fn = function(v)
@@ -63,12 +67,12 @@ function Binding:subscribe(callback)
         return function()
             GObject.signal_handler_disconnect(self.emitter, id)
         end
-    elseif type(self.emitter.subscribe) == "function" then
+    elseif type(self.emitter.subscribe) == 'function' then
         return self.emitter:subscribe(function()
             callback(self:get())
         end)
     else
-        error("can not subscribe: Not a GObject or a Variable " .. tostring(self))
+        error('can not subscribe: Not a GObject or a Variable ' .. tostring(self))
     end
 end
 
--- astal/process.lua
+++ astal/process.lua
@@ -1,9 +1,11 @@
-local lgi = require("lgi")
-local Astal = lgi.require("AstalIO", "0.1")
+local lgi = require('lgi')
+
+---@type AstalIO
+local AstalIO = lgi.require('AstalIO', '0.1')
 
 local M = {}
 
-M.Process = Astal.Process
+M.Process = AstalIO.Process
 
 ---@param commandline string | string[]
 ---@param on_stdout? fun(out: string): nil
@@ -11,19 +13,19 @@ M.Process = Astal.Process
 ---@return { kill: function } | nil proc
 function M.subprocess(commandline, on_stdout, on_stderr)
     on_stdout = on_stdout or function(out)
-        io.stdout:write(tostring(out) .. "\n")
+        io.stdout:write(tostring(out) .. '\n')
     end
 
     on_stderr = on_stderr or function(err)
-        io.stderr:write(tostring(err) .. "\n")
+        io.stderr:write(tostring(err) .. '\n')
     end
 
     local proc, err
 
-    if type(commandline) == "table" then
-        proc, err = Astal.Process.subprocessv(commandline)
+    if type(commandline) == 'table' then
+        proc, err = AstalIO.Process.subprocessv(commandline)
     else
-        proc, err = Astal.Process.subprocess(commandline)
+        proc, err = AstalIO.Process.subprocess(commandline)
     end
 
     if err ~= nil then
@@ -42,10 +44,10 @@ end
 ---@param commandline string | string[]
 ---@return string, string
 function M.exec(commandline)
-    if type(commandline) == "table" then
-        return Astal.Process.execv(commandline)
+    if type(commandline) == 'table' then
+        return AstalIO.Process.execv(commandline)
     else
-        return Astal.Process.exec(commandline)
+        return AstalIO.Process.exec(commandline)
     end
 end
 
@@ -55,20 +57,20 @@ function M.exec_async(commandline, callback)
     callback = callback
         or function(out, err)
             if err ~= nil then
-                io.stderr:write(tostring(out) .. "\n")
+                io.stderr:write(tostring(out) .. '\n')
             else
-                io.stdout:write(tostring(err) .. "\n")
+                io.stdout:write(tostring(err) .. '\n')
             end
         end
 
-    if type(commandline) == "table" then
-        Astal.Process.exec_asyncv(commandline, function(_, res)
-            local out, err = Astal.Process.exec_asyncv_finish(res)
+    if type(commandline) == 'table' then
+        AstalIO.Process.exec_asyncv(commandline, function(_, res)
+            local out, err = AstalIO.Process.exec_asyncv_finish(res)
             callback(out, err)
         end)
     else
-        Astal.Process.exec_async(commandline, function(_, res)
-            local out, err = Astal.Process.exec_finish(res)
+        AstalIO.Process.exec_async(commandline, function(_, res)
+            local out, err = AstalIO.Process.exec_finish(res)
             callback(out, err)
         end)
     end
