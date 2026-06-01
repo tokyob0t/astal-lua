6c525456f9d872e64136ffbf44707ef62065a4ac
Kevin <tokyob0t.business@proton.me>
2026-05-31 21:39:52 -0400

    chore(astal.gtk3.init): update types

--- astal/gtk3/init.lua
+++ astal/gtk3/init.lua
@@ -5,7 +5,10 @@ return {
     Widget = require('astal.gtk3.widget'),
     App = require('astal.gtk3.app'),
 
+    ---@type Gtk
     Gtk = lgi.require('Gtk', '3.0'),
+    ---@type Gdk
     Gdk = lgi.require('Gdk', '3.0'),
+    ---@type Astal
     Astal = lgi.require('Astal', '3.0'),
 }
