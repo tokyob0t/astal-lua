ee16817ea8578a5dabffff350381c8c1e7031984
Kevin <tokyob0t.business@proton.me>
2026-06-19 17:08:45 -0400

    feat(binding): nested bindings, operators overload

--- astal/binding.lua
+++ astal/binding.lua
@@ -2,16 +2,22 @@ local lgi = require('lgi')
 local GObject = lgi.require('GObject', '2.0')
 local Gio = lgi.require('Gio', '2.0')
 
----@class AstalLuaBinding: table
----@field emitter table | AstalLuaVariable | GObject.Object
+---@class AstalLua.Binding<T>
+---@field emitter table | AstalLua.Variable | GObject.Object
 ---@field property? string
 ---@field private transform_fn function
----@field private __index AstalLuaBinding
----@overload fun(emitter: GObject.Object, property: string): AstalLuaBinding
----@overload fun(emitter: AstalLuaVariable): AstalLuaBinding
----@overload fun(emitter: { subscribe: function, get: function }): AstalLuaBinding
+---@field private __index AstalLua.Binding
+---@operator add(number | AstalLua.Binding<number> ): AstalLua.Binding<number>
+---@operator sub(number | AstalLua.Binding<number> ): AstalLua.Binding<number>
+---@operator mul(number | AstalLua.Binding<number> ): AstalLua.Binding<number>
+---@operator div(number | AstalLua.Binding<number> ): AstalLua.Binding<number>
+---@operator unm: AstalLua.Binding<number>
+---@operator concat(string | AstalLua.Binding<string> ): AstalLua.Binding<string>
+---@overload fun(emitter: GObject.Object, property: string): AstalLua.Binding<any>
+---@overload fun(emitter: AstalLua.Variable<`T`>): AstalLua.Binding<T>
+---@overload fun(emitter: { subscribe: function, get: function }): AstalLua.Binding<any>
 local Binding = {}
-Binding.__index = Binding ---@diagnostic disable-line
+Binding.__index = Binding
 Binding.__type = 'Binding'
 
 function Binding:is_type_of(object)
@@ -20,9 +26,17 @@ end
 
 ---@param emitter GObject.Object
 ---@param property string
----@overload fun(emitter: AstalLuaVariable): AstalLuaBinding
----@overload fun(emitter: { subscribe: function, get: function }): AstalLuaBinding
+---@overload fun(emitter: AstalLua.Variable): AstalLua.Binding
+---@overload fun(emitter: { subscribe: function, get: function }): AstalLua.Binding
 function Binding.new(emitter, property)
+    if not property then
+        assert(emitter.get, 'can not get: Not a GObject or a Variable ' .. tostring(emitter))
+        assert(
+            emitter.subscribe,
+            'can not subscribe: Not a GObject or a Variable ' .. tostring(emitter)
+        )
+    end
+
     return setmetatable({
         emitter = emitter,
         property = property,
@@ -32,29 +46,154 @@ function Binding.new(emitter, property)
     }, Binding)
 end
 
----@private
+local function constant(v)
+    return Binding.new {
+        get = function()
+            return v
+        end,
+        subscribe = function()
+            return function() end
+        end,
+    }
+end
+
+local function coerce(v)
+    if Binding:is_type_of(v) then
+        return v
+    end
+
+    return constant(v)
+end
+
+---@protected
+function Binding:__call(emitter, prop) end
+
+---@protected
 function Binding:__tostring()
     local str = 'Binding<' .. tostring(self.emitter)
-    if self.property ~= nil then
+    if self.property then
         str = str .. ', ' .. self.property
+    else
+        str = str .. ', ' .. string.format('%q', self:get())
     end
     return str .. '>'
 end
 
----@return any
-function Binding:get()
-    if self.property ~= nil and Gio.Settings:is_type_of(self.emitter) then
-        return self.transform_fn(self.emitter:get_value(self.property))
-    elseif self.property ~= nil and GObject.Object:is_type_of(self.emitter) then
-        return self.transform_fn(self.emitter[self.property])
-    elseif type(self.emitter.get) == 'function' then
-        return self.transform_fn(self.emitter:get())
-    else
-        error('can not get: Not a GObject or a Variable ' .. tostring(self))
-    end
+---@protected
+function Binding.__concat(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 .. v2
+    end)
 end
 
----@param transform fun(value: any): any
+---@protected
+function Binding.__add(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 + v2
+    end)
+end
+
+---@protected
+function Binding.__sub(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 - v2
+    end)
+end
+
+---@protected
+function Binding.__mul(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 * v2
+    end)
+end
+
+---@protected
+function Binding.__div(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 / v2
+    end)
+end
+
+---@protected
+function Binding.__unm(b1)
+    return b1:as(function(v)
+        return -v
+    end)
+end
+
+---@protected
+function Binding.__mod(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 % v2
+    end)
+end
+
+---@protected
+function Binding.__pow(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return v1 ^ v2
+    end)
+end
+
+---@protected
+function Binding.__idiv(b1, b2)
+    local _b1 = coerce(b1)
+    local _b2 = coerce(b2)
+
+    return Binding.derive({ _b1, _b2 }, function(v1, v2)
+        return math.floor(v1 / v2)
+    end)
+end
+
+---@return any
+function Binding:get()
+    local utils = require('astal._utils')
+
+    if not self.property then
+        return self.transform_fn(self.emitter:get())
+    end
+
+    local current = self.emitter
+
+    for prop in self.property:gmatch('[^.]+') do
+        if type(current) == 'nil' then
+            return self.transform_fn()
+        elseif Gio.Settings:is_type_of(current) then
+            current = utils.decode_variant(current:get_value(prop))
+        elseif GObject.Object:is_type_of(current) then
+            current = current[prop]
+        else
+            error('can not get: Not a GObject at property ' .. prop .. ' ' .. tostring(current))
+        end
+    end
+
+    return self.transform_fn(current)
+end
+
+---@generic T
+---@param transform fun(value: any): T
+---@return AstalLua.Binding<T>
 function Binding:as(transform)
     local b = Binding.new(self.emitter, self.property)
     b.transform_fn = function(v)
@@ -66,9 +205,15 @@ end
 ---@param callback fun(value: any)
 ---@return function
 function Binding:subscribe(callback)
-    if self.property ~= nil and Gio.Settings:is_type_of(self.emitter) then
-        local id = self.emitter.on_changed:connect(function(_, k)
-            if k == self.property then
+    if not self.property then
+        return self.emitter:subscribe(function()
+            callback(self:get())
+        end)
+    end
+
+    if Gio.Settings:is_type_of(self.emitter) then
+        local id = self.emitter.on_changed:connect(function(_, key)
+            if key == self.property then
                 callback(self:get())
             end
         end)
@@ -76,20 +221,98 @@ function Binding:subscribe(callback)
         return function()
             GObject.signal_handler_disconnect(self.emitter, id)
         end
-    elseif self.property ~= nil and GObject.Object:is_type_of(self.emitter) then
-        local id = self.emitter.on_notify:connect(function()
-            callback(self:get())
-        end, self.property, false)
-        return function()
-            GObject.signal_handler_disconnect(self.emitter, id)
-        end
-    elseif type(self.emitter.subscribe) == 'function' then
-        return self.emitter:subscribe(function()
-            callback(self:get())
-        end)
-    else
-        error('can not subscribe: Not a GObject or a Variable ' .. tostring(self))
     end
+
+    local chain_unsubs = {}
+    local properties = {}
+
+    for part in self.property:gmatch('[^.]+') do
+        table.insert(properties, part)
+    end
+
+    local subscribe_level = function(start_idx)
+        for i = start_idx, #chain_unsubs do
+            if chain_unsubs[i] then
+                chain_unsubs[i]()
+                chain_unsubs[i] = nil
+            end
+        end
+
+        local current = self.emitter
+
+        for i = 1, start_idx - 1 do
+            if current == nil then
+                return
+            end
+            current = current[properties[i]]
+        end
+
+        for i = start_idx, #properties do
+            if current == nil then
+                break
+            end
+
+            local prop = properties[i]
+            local is_last = (i == #properties)
+            local obj = current
+
+            local id = obj.on_notify:connect(function()
+                if not is_last then
+                    subscribe_level(i + 1)
+                end
+                callback(self:get())
+            end, prop, false)
+
+            chain_unsubs[i] = function()
+                GObject.signal_handler_disconnect(obj, id)
+            end
+
+            if not is_last then
+                current = current[prop]
+            end
+        end
+    end
+
+    subscribe_level(1)
+
+    return function()
+        for _, unsub in ipairs(chain_unsubs) do
+            if unsub then
+                unsub()
+            end
+        end
+    end
+end
+
+function Binding.derive(deps, transform)
+    return Binding.new {
+        get = function()
+            local values = {}
+
+            for i, dep in ipairs(deps) do
+                values[i] = dep:get()
+            end
+
+            return transform(table.unpack(values, 1, #deps))
+        end,
+        subscribe = function(self, callback)
+            local unsubs = {}
+
+            local function update()
+                callback(self:get())
+            end
+
+            for i, dep in ipairs(deps) do
+                unsubs[i] = dep:subscribe(update)
+            end
+
+            return function()
+                for _, unsub in ipairs(unsubs) do
+                    unsub()
+                end
+            end
+        end,
+    }
 end
 
 ---@diagnostic disable-next-line
